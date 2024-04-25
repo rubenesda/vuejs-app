2b3a70f2995cd4302884de190b1bdae9d5dac306
Ruben Dario Escandon <rescandon@vovsolutions.com>
2024-04-24 21:49:02 -0500

    add(handle-array): add vue directive 'v-for' for managing arrays and refactor App.vue file and main.css file

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const header = ref('Shopping List App')
+const items = ref([
+  { id: '1', label: '10 party hats' },
+  { id: '1', label: '2 board games' },
+  { id: '1', label: '20 cups' }
+])
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ header }}</h1>
+  <ul>
+    <!-- Uncomment this line code if you care for nondestructure form -->
+    <!-- <li v-for="item in items" :key="item.id">{{ item.label }}</li> -->
+    <!-- Below, it is using destructure form -->
+    <li v-for="({ id, label }, index) in items" :key="id">
+      {{ index }}
+      {{ label }}
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
