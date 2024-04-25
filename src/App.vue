c6f18a3fce1b4144fde5708b459db27770a7e24e
Ruben Dario Escandon <rescandon@vovsolutions.com>
2024-04-24 22:10:03 -0500

    return(handle-array): roll back managing array variables insted of object variables

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 import { ref } from 'vue'
 
 const header = ref('Shopping List App')
-const items = ref({
-  'item-1': { id: '1', label: '10 party hats' },
-  'item-2': { id: '1', label: '2 board games' },
-  'item-3': { id: '1', label: '20 cups' }
-})
+const items = ref([
+  { id: '1', label: '10 party hats' },
+  { id: '1', label: '2 board games' },
+  { id: '1', label: '20 cups' }
+])
 </script>
 
 <template>
@@ -15,11 +15,7 @@ const items = ref({
     <!-- Uncomment this line code if you care for nondestructure form -->
     <!-- <li v-for="item in items" :key="item.id">{{ item.label }}</li> -->
     <!-- Below, it is using destructure form -->
-    <!-- Now. index variable are rendering the prepend 'item-#' for every object key from our object variable -->
-    <li v-for="({ id, label }, index) in items" :key="id">
-      {{ index }}
-      {{ label }}
-    </li>
+    <li v-for="{ id, label } in items" :key="id">{{ label }}</li>
   </ul>
 </template>
 
